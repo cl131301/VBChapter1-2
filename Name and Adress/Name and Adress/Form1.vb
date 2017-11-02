7619b037fd2ed9aa832b3f3d16b817062c40971a
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-02 10:28:54 -0500

    Updated Name and Adress

---
+++ Name and Adress/Name and Adress/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class lblNameAndAdress
+
+    Private Sub btnShowInfo_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+        lblCityStateZip.Visible = True
+        lblName.Visible = True
+        lblStreet.Visible = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
